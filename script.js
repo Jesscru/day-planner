8ab44d72f23aa21d87a25a4a894b401170a5ccb3
jesscru <jessicacrump7@gmail.com>
2020-11-07 22:14:02 -0700

    modified spacing of textarea, changed title, added date to display

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ var save = $('.saveBtn');
 var userEntry = $('.text-area')
 
 
+// displays the current day 
+$('#currentDay').text(moment().format("dddd, MMMM Do YYYY"));
+
+
 // when user clicks save buttons entered data in the text field will be saved to local storage 
 // if nothing is in the field and the user clicks save, they will be sent an alert to enter a value
 save.on('click', function(event){
     event.preventDefault();
 
     if (timeBlock !== null) {
-        localStorage.setItem($(this).val()), timeBlock.html;
+        localStorage.setItem($(this).val(), ENTERTEXTAREARECEPTIONHERE);
         // var displayEvent = localStorage.getItem()
     } else {
         alert("Please enter the event you would like to schedule for this hour before saving.");
