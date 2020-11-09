e1e3e4fe7600d6fffb69cd8a45a51fb0459e7621
jesscru <jessicacrump7@gmail.com>
2020-11-08 20:51:19 -0700

    fixed sapacing on save buttons; working on getting local storage to save and diaply--not yet working though

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var timeBlock = $('.time-block');
 var hour = $('.hour');
 var save = $('.saveBtn');
-var userEntry = $('.text-area')
+var textArea = $('.text-area');
+var userEntry = JSON.stringify($('.text-area').value);
 
 
 // displays the current day 
@@ -10,32 +11,57 @@ $('#currentDay').text(moment().format("dddd, MMMM Do YYYY"));
 // sets colors of the textareas based on the time 
 // past is purple
 if (moment().hour() < save.val()) {
-    userEntry.attr('class', 'past')
+    textArea.attr('class', 'past');
 } 
 // present is orange
 if (moment().hour() === save.val()) {
-    userEntry.attr('class', 'present')
+    textArea.attr('class', 'present');
 } 
 // future is green 
 if (moment().hour() > save.val()) {
-    userEntry.attr('class', 'future')
+    textArea.attr('class', 'future');
 } 
 
-console.log(save.val());
-console.log(moment().hour());
 
-// // when user clicks save buttons entered data in the text field will be saved to local storage 
-// // if nothing is in the field and the user clicks save, they will be sent an alert to enter a value
+
+// when user clicks save buttons entered data in the text field will be saved to local storage 
+// if nothing is in the field and the user clicks save, they will be sent an alert to enter a value
 // save.on('click', function(event){
 //     event.preventDefault();
 
-//     if (timeBlock !== null) {
-//         localStorage.setItem($(this).val(), $('.text-area').value);
-//         // var displayEvent = localStorage.getItem()
+//     if (userEntry !== null) {
+//         localStorage.setItem($(this).val(), userEntry);
+//         var displayEvent = localStorage.getItem($(this).val);
+//         userEntry.text(localStorage.getItem($(this).val()));
 //     } else {
 //         alert("Please enter the event you would like to schedule for this hour before saving.");
 //     }
 // })
 
 
-console.log();
+
+save.on('click', function(event){
+    var hourArray = [];
+    event.preventDefault();
+
+    if (userEntry !== '') {
+        hourArray.push($(this).val());
+        localStorage.setItem($(this).val(), userEntry);
+       
+
+        for (var i = 0; i<hourArray.length; i++) {
+          if ($(this).val() === $('name')) {
+            var displayEvent = localStorage.getItem($(this).val);
+            userEntry.text(displayEvent);
+          }
+        }
+
+    } else {
+        alert("Please enter the event you would like to schedule for this hour before saving.");
+    }
+    
+    console.log(userEntry);
+
+    console.log(hourArray);
+})
+
