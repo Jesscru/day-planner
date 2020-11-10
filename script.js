d43beb4cd9a221f4f242d32ae854baea3a60ffae
jesscru <jessicacrump7@gmail.com>
2020-11-09 18:42:56 -0700

    made colors work properly with time

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var timeBlock = $('.time-block');
 var hour = $('.hour');
+var textAreaName = $('input').attr('name');
 var save = $('.saveBtn');
 var clear = $('.clearBtn');
 var textArea = $('.text-area');
-var userEntry = $.trim($('.text-area').val());
+// var userEntry = $('input').value;
 var hourArray = [];
 
 
@@ -11,36 +12,43 @@ var hourArray = [];
 $('#currentDay').text(moment().format("dddd, MMMM Do YYYY"));
 
 // sets colors of the textareas based on the time 
-// past is purple
-if (moment().hour() < save.val()) {
-    textArea.attr('class', 'past');
-} 
-// present is orange
-if (moment().hour() === save.val()) {
-    textArea.attr('class', 'present');
-} 
-// future is green 
-if (moment().hour() > save.val()) {
-    textArea.attr('class', 'future');
-} 
+        // past is purple
+    for (var i = 9; i < 18; i++) {
+        if (parseInt(moment().hours()) > i) {
+            $('#' + i).attr('class', 'past');
+        }   // present is orange
+         else if (parseInt(moment().hours()) === i) {
+            $('#' + i).attr('class', 'present');
+        }   // future is green 
+         else {
+            $('#' + i).attr('class', 'future');
+        }
+    }
+    
 
+
+console.log(parseInt(moment().hours()));
 
 
 // when user clicks save buttons entered data in the text field will be saved to local storage 
 // if nothing is in the field and the user clicks save, they will be sent an alert to enter a value
 save.on('click', function(event){
     event.preventDefault();
+     var hours = $(this).val()  
+     var userEntry = $('#' + hours).val().trim();
+    console.log(userEntry)
+    
+    hourArray.push(userEntry);
 
-    if (textArea !== undefined) {
-        // hourArray.push(userEntry);
-        localStorage.setItem($(this).val(), $(textArea).val());
-        var displayEvent = localStorage.getItem($(this).val());
-        textArea.text(displayEvent);
+    if (userEntry !== null) {
+        localStorage.setItem($(this).val(), userEntry);
     } else {
         alert("Please enter the event you would like to schedule for this hour before saving.");
     }
 })
 
+
+// clears data from local storage and thus, the page when the clear button is clicked
 clear.on('click', function(event){
     event.preventDefault();
 
@@ -60,7 +68,7 @@ clear.on('click', function(event){
        
 
 //         for (var i = 0; i<hourArray.length; i++) {
-//           if ($(this).val() === $('name')) {
+//           if (save.val() === $('name')) {
 //             var displayEvent = localStorage.getItem($(this).val);
 //             userEntry.text(displayEvent);
 //           }
@@ -70,9 +78,3 @@ clear.on('click', function(event){
     // else {
     //     alert("Please enter the event you would like to schedule for this hour before saving.");
     // }
-    
-    
-    
-    console.log(hourArray);
-    console.log($($('this')).val());
-
